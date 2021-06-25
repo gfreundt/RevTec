baed89ca64b6dfdb0ae62ee9a8df911a05f24f02
Gabriel <gfreundt@gmail.com>
2021-06-25 08:28:36 -0500

    Major updates

--- scraper.py
+++ scraper.py
@@ -1,40 +1,36 @@
 from datetime import datetime as dt
 import urllib.request
 import csv, time, json
-from PIL import Image
-import pytesseract
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as WebDriverOptions
 from selenium.webdriver.support.select import Select
-import requests
 import os
+from google.cloud import vision
+import io
+import sys
 
 
 def consolidate():
+    print('Consolidando bases de datos...')
     with open('placas.txt', mode='r', encoding='utf-8') as file:
         data_base = [i.strip() for i in file.readlines()]
-        print(len(data_base))
     with open('resultados.txt', mode='r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter='|')
         data_resultados = [i[0] for i in reader]
-        print(len(data_resultados))
+        r = len(data_resultados)
     with open('placas.txt', mode='w', encoding='utf-8') as file:
         data_pendiente = [f'{i}\n' for i in data_base if i not in data_resultados]
         file.writelines(data_pendiente)
-        print(len(data_pendiente))
-
-
-def get_ocr_keys():
-    with open('ocr_keys.json', mode='r') as file:
-        data = json.load(file)['data']
-        keys = [i['key'] for i in data]
-    return keys
+        s = len(data_pendiente)
+    print(f'Completas: {r} / Totales: {s}  =  {r/s*100:.2f}%')
 
 
 def list_of_pending_placas():
-    with open('placas.txt', mode='r', encoding='utf-8') as file:
-        return [i.strip() for i in file.readlines()]
+    with open('placas'+str(session)+'.txt', mode='r', encoding='utf-8') as file:
+        p = [i.strip() for i in file.readlines()]
+        return p
+
 
 
 def driver_options():
@@ -50,22 +46,22 @@ def driver_options():
     return options
 
 
-def extract(placa, url, ocr_key):
+def extract(placa, url):
     driver = webdriver.Chrome('/usr/bin/chromedriver', options = driver_options())
     while True:  # Loop infinito hasta salir con placa consultada
-        miss_counter = 0
         captcha = ""
         while not captcha:
             # Activar página web
             driver.get(url)
             # Obtener y grabar imagen del captcha
-            urllib.request.urlretrieve(driver.find_element_by_xpath('//img').get_attribute('src'), 'captcha.jpg')
+            urllib.request.urlretrieve(driver.find_element_by_xpath('//img').get_attribute('src'), 'captcha'+str(session)+'.jpg')
             # Leer imagen de captcha grabado en disco
-            captcha = ocr2('captcha.jpg', api_key=ocr_key)
-            if miss_counter == 10:
-                raise Exception # force an error to switch to the next OCR key
-            else:
-                miss_counter += 1
+            #captcha = ocr2('captcha.jpg', api_key=ocr_key)
+            captcha = ocr3('captcha'+str(session)+'.jpg')
+            #if miss_counter == 10:
+            #    raise Exception # force an error to switch to the next OCR key
+            #else:
+            #    miss_counter += 1
         driver.find_element_by_id('txtPlaca').send_keys(placa)      # Llenar campo "Nro de placa"
         driver.find_element_by_id('txtCaptcha').send_keys(captcha)  # Llenar campo de Captcha
         driver.find_element_by_id('BtnBuscar').click()              # Click Botón Buscar
@@ -93,24 +89,14 @@ def save_captcha(captcha):
             return
 
 
-def ocr2(image_path, api_key, overlay=False, language='eng'):   # API
-    payload = {'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               }
-    with open(image_path, 'rb') as f:
-        r = requests.post('https://api.ocr.space/parse/image',
-                          files={image_path: f},
-                          data=payload,
-                          )
-    respuesta = r.content.decode()
-
-    if "ParsedText" in respuesta:
-        pos0 = respuesta.index("ParsedText") + 13
-        pos1 = respuesta.index('"',pos0)
-        return respuesta[pos0:pos1-4]
-    else:
-        return ""
+def ocr3(image_path):
+    client = vision.ImageAnnotatorClient.from_service_account_json('google-vision-keys.json')
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    return texts[0].description.strip()
 
 
 def analizar_respuesta(placa,respuesta):
@@ -142,28 +128,24 @@ def add_to_file(filename, result):
 
 
 def main():
-    ocr_keys = get_ocr_keys()
-    use_ocr_key = ocr_keys[0]
-    ocr_key_index = 0
     url = 'https://portal.mtc.gob.pe/reportedgtt/form/frmconsultaplacaitv.aspx'
-    
     for k, placa in enumerate(list_of_pending_placas()):
+        print(f'({session}){k} Placa:{placa}|', end='')
         try:
-            print(k, placa,"|",end="")
-            scrape = extract(placa, url, use_ocr_key)
+            scrape = extract(placa, url)
             resultado = analizar_respuesta(placa, scrape)
-            add_to_file('resultados.txt', resultado)
+            add_to_file('resultados'+str(session)+'.txt', resultado)
+        except KeyboardInterrupt:
+            quit()
         except:
-            ocr_key_index += 1
-            if ocr_key_index < len(ocr_keys):
-                use_ocr_key = ocr_keys[ocr_key_index]
-                print('Cambio de OCR Key')
-            else:
-                print('End of OCR keys')
-                return
+            pass
 
 
 
+try:
+    session = sys.argv[1] 
+except:
+    consolidate()
+    quit()
 
-consolidate()
 main()
