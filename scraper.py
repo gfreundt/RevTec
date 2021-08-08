ca5f94ee52e4c6dbb1c97c77d73ca668055b9161
Gabriel <gfreundt@gmail.com>
2021-08-08 15:36:57 -0500

    EasyOCR only, no handling of Captcha images.

--- scraper.py
+++ scraper.py
@@ -5,34 +5,31 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as WebDriverOptions
 from selenium.webdriver.support.select import Select
-from google.cloud import vision
 import threading
 from tqdm import tqdm
-from PIL import Image
-import hashlib
-import gpyocr
+import easyocr
 
 
 class Basics:
     def __init__(self, ses):
-        self.monitor = [0 for _ in range(ses)]
-        #self.captcha_table_data, self.captcha_table_keys = self.load_captcha_hashes()
-        self.webdriver_options = self.load_webdriver_options()
+        # Web scraping information
         self.url = 'https://portal.mtc.gob.pe/reportedgtt/form/frmconsultaplacaitv.aspx'
+        self.webdriver_options = self.load_webdriver_options()
+        # Define paths
+        self.base_dir = '/home/gabriel/pythonCode/RevTec/'
+        self.temp_dir = os.path.join(self.base_dir, 'temp')
+        self.sorted_images_dir = os.path.join(self.base_dir, 'sortedImages')
+        self.unsorted_images_dir = os.path.join(self.base_dir, 'unsortedImages')   #unsortedImages
+        # Define blank counters
+        self.monitor = [0 for _ in range(ses)]
         self.exceptions = 0
-        self.next_pending_image = len(os.listdir('/home/gabriel/pythonCode/RevTec/pending_images/')) + 1
+        self.results = [[] for _ in range(ses)]
+        self.done = [False for _ in range(ses)]
 
-    def load_captcha_hashes(self):
-        with open('/home/gabriel/pythonCode/RevTec/captcha_hashes.txt', mode='r', encoding='utf-8') as hash_file:
-            hashes = hash_file.readlines()
-        captcha_table = {h.split('.')[0].strip():int(h.split('.')[1]) for h in hashes}
-        return captcha_table, captcha_table.keys()
-    
     def load_webdriver_options(self):
         options = WebDriverOptions()
         options.add_argument("--window-size=800,800")
         options.add_argument("--headless")
-        #options.add_argument("--disable-gpu")
         options.add_argument("--silent")
         options.add_argument("--disable-notifications")
         options.add_argument("--incognito")
@@ -40,80 +37,67 @@ class Basics:
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         return options
 
-
-def split(q):
-    with open('todo_placas.txt', mode='r', encoding='utf-8') as file:
+def split(ses, iters):
+    with open(os.path.join(RUN.base_dir, 'todo_placas.txt'), mode='r', encoding='utf-8') as file:
             data_base = [i.strip() for i in file.readlines()]
-            records = len(data_base)
-            block = records//q
-    for n in range(q):
-        with open('placas' + str(n) + '.txt', mode='w', encoding='utf-8') as file:
+            block = len(data_base)//ses
+    for n in range(ses):
+        with open(os.path.join(RUN.temp_dir, 'placas' + str(n) + '.txt'), mode='w', encoding='utf-8') as file:
             data_pendiente = [f'{i}\n' for i in data_base[block*n:block*(n+1)]]
-            file.writelines(data_pendiente)
+            file.writelines(data_pendiente[:iters])
 
-
-def consolidate(q):
-    for i in range(q):
-        filenum = str(i)
-        with open('placas'+filenum+'.txt', mode='r', encoding='utf-8') as file:
-            data_base = [i.strip() for i in file.readlines()]
-        with open('resultados'+filenum+'.txt', mode='r', encoding='utf-8') as file:
-            reader = csv.reader(file, delimiter='|')
-            data_resultados = [i[0] for i in reader]
-            r = len(data_resultados)
-        with open('placas'+filenum+'.txt', mode='w', encoding='utf-8') as file:
-            data_pendiente = [f'{i}\n' for i in tqdm(data_base, desc='File'+str(i)) if i not in data_resultados]
-            file.writelines(data_pendiente)
-            s = len(data_pendiente)
-        print(f'File: {i} - Completas: {r} / Totales: {s}  =  {r/s*100:.2f}%')
-
+def consolidate():
+    resultados= []
+    # Integrate all partial results into main file
+    with open(os.path.join(RUN.base_dir,'todo_resultados.txt'), mode='a', encoding='utf-8') as file:
+        for result in RUN.results:
+            for item in result:
+                writer = csv.writer(file, delimiter="|")
+                writer.writerow(item)
+                resultados.append(item[0])
+    # Cross-reference placas and resultados files and only keep placas with no results
+    with open(os.path.join(RUN.base_dir, 'todo_placas.txt'), mode='r', encoding='utf-8') as file:
+        placas = [i.strip() for i in file.readlines()]
+        placas_left = [f'{i}\n' for i in tqdm(placas, desc='Purging Placas database') if i not in resultados]
+    with open(os.path.join(RUN.base_dir, 'todo_placas.txt'), mode='w', encoding='utf-8') as file:
+        file.writelines(placas_left)
 
 def list_of_pending_placas(ses):
-    with open('placas'+ses+'.txt', mode='r', encoding='utf-8') as file:
-        p = [i.strip() for i in file.readlines()]
-        return p
-
+    with open(os.path.join(RUN.temp_dir,'placas'+ses+'.txt'), mode='r', encoding='utf-8') as file:
+        return [i.strip() for i in file.readlines()]
 
 def extract(placa, url, ses):
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options = run.webdriver_options)
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options = RUN.webdriver_options)
     while True:  # Loop infinito hasta salir con placa consultada
         captcha = ""
         while not captcha:
             driver.get(url)
-            urllib.request.urlretrieve(driver.find_element_by_xpath('//img').get_attribute('src'), 'captcha'+ses+'.jpg')
-            
-            # See if hash in database to avoid using OCR
-            #captcha_hash = hashlib.md5(Image.open('captcha'+ses+'.jpg').tobytes())
-            #captcha_hash = captcha_hash.hexdigest()
-            #if captcha_hash in captcha_table_keys:
-            #    captcha = captcha_table_data[captcha_hash]
-            #    print('*** Woohoo ***')
-            #else:  # If hash not in database, go with OCR
-            captcha = ocr4('captcha'+ses+'.jpg')
-            if not captcha:
-                # Copy captcha into directory to be processed some other time
-                os.system('cp captcha'+ses+'.jpg ' + os.path.join('pending_images',f'pend{run.next_pending_image:06}.jpg'))
-                run.next_pending_image += 1
-
-                
+            # Retrieve captcha image from web and save in temp folder as generic 'captcha#.jpg'
+            captcha_filename = os.path.join(RUN.temp_dir,'captcha'+str(ses)+'.jpg')
+            urllib.request.urlretrieve(driver.find_element_by_xpath('//img').get_attribute('src'), captcha_filename)
+            captcha = ocr(captcha_filename)
+            if not valid_captcha(captcha):
+                captcha = None
         driver.find_element_by_id('txtPlaca').send_keys(placa)      # Llenar campo "Nro de placa"
         driver.find_element_by_id('txtCaptcha').send_keys(captcha)  # Llenar campo de Captcha
         driver.find_element_by_id('BtnBuscar').click()              # Click Botón Buscar
         time.sleep(0.5)
         respuestas = [i.text for i in driver.find_elements_by_class_name('gridItemGroup')]  # Recoge resultados
         if respuestas:      # Datos completos
-            # Save captcha correctly identified
-            os.system('cp --backup=numbered captcha'+ses+'.jpg ' +  os.path.join("images",captcha+".jpg"))
             driver.quit()
             return respuestas
         elif "no es correcto" in driver.find_element_by_id('lblAlertaMensaje').text:  # Error en captcha ingresado
             pass
         else: # No se obtuvo datos de la placa
-            # Save captcha correctly identified
-            os.system('cp --backup=numbered captcha'+ses+'.jpg ' +  os.path.join("images",captcha+".jpg"))
             driver.quit()
             return None
 
+def valid_captcha(captcha):
+    if len(captcha) != 6:
+        return False
+    elif not captcha.isdigit():
+        return False
+    return True
 
 def valid_placa(placa):
     if len(placa) != 6:
@@ -123,28 +107,13 @@ def valid_placa(placa):
     if placa[3].isalpha() or placa[4].isalpha() or placa[5].isalpha():
         return False
     return True
-    
 
-def ocr4(image_path):
-    try:
-        r = gpyocr.tesseract_ocr(image_path, psm=7)[0].strip()
-        captcha_guessed = ''.join([i for i in r if i.isdigit()])
-        if len(captcha_guessed) == 6:
-            return captcha_guessed
-        else:
-            return ''
-    except:
+def ocr(image_path):
+    result = reader.readtext(image_path)
+    if len(result) > 0 and len(result[0]) > 0:
+        return result[0][1]
+    else:
         return ''
-
-def ocr3(image_path):
-    client = vision.ImageAnnotatorClient.from_service_account_json('google-vision-keys.json')
-    with io.open(image_path, 'rb') as image_file:
-        content = image_file.read()
-    image = vision.Image(content=content)
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    return texts[0].description.strip()
-
 
 def analizar_respuesta(placa,respuesta):
     fecha_hoy = dt.strftime(dt.now(),"%m/%d/%Y")
@@ -167,168 +136,98 @@ def analizar_respuesta(placa,respuesta):
     tipo = tercera_linea[1]
     return [placa,empresa,cert,fecha_inspec,fecha_finvig,aprobacion,estado_vigencia,ambito,tipo,fecha_hoy]
 
-
 def add_to_file(filename, result):
     with open(filename, mode='a', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter="|")
         writer.writerow(result)
 
-
 def thread_monitor(ses, iterations):
     print(f'{" Progress ":-^{ses*8+1}}')
     while True:
-        monitor = [i/(iterations+1) for i in run.monitor]
+        monitor = [i/iterations for i in RUN.monitor]
         p = ''
         for mon in monitor:
             p += '| ' + f'{mon:05.1%}' + ' '
-        p += '| Running: ' + str(sum(run.monitor)-ses+1)
+        p += '| Running: ' + str(sum(RUN.monitor))
         print(p, end='\r', flush=True)
-        if monitor.count(1) == ses:
+        if all(RUN.done):
             print('\n')
             return
         time.sleep(2)
 
-
-def dashboard(start, opt=True):
-    total = len(os.listdir(IMAGE_FOLDER))
+def dashboard(start, exit=True):
     # Count records for done and pending
     with open('todo_placas.txt', mode='r') as file:
         placas = len([i for i in file.readlines()])
     with open('todo_resultados.txt', mode='r') as file:
         resultados = len([i for i in file.readlines()])
-
+    # Basic data
     print('*'*10, 'Stats', '*'*10)
-    print(f'Total Captchas: {total:,}')
     print(f'Total Results: {resultados:,} ({resultados/placas*100:.2f}%)\nPlacas Pending: {placas:,}')
-    if opt:
+    if exit:
         records = int(sys.argv[1]) * int(sys.argv[2])
         time_taken = dt.now() - start
-        print(f'Total Exceptions: {run.exceptions:,} = {run.exceptions/records:.2%}')
-        print(f'Process Rate: {records:,} records in {time_taken} = {records/time_taken.total_seconds():.2f} records/second')
+        print(f'Total Exceptions: {RUN.exceptions:,} = {RUN.exceptions/records:.2%}')
+        print(f'Process Rate: {records*(repeat+1):,} records in {time_taken} = {records*(repeat+1)/time_taken.total_seconds():.2f} records/second')
     print('*'*10, 'End Stats', '*'*10)
 
-
-def cleaner(fpath):
-    # Clean captchas without 'jpg' in name
-    for f in os.listdir(IMAGE_FOLDER):
-        if 'jpg' not in f:
-            os.remove(os.path.join(IMAGE_FOLDER,f))
-    # Eliminate duplicates from placas source file
-    with open(fpath, mode='r', encoding='utf-8') as file:
-        all_data = set([i.strip() for i in file.readlines()])
-        pure = [i for i in tqdm(all_data) if valid_placa(i)]
-    with open(fpath, mode='w', encoding='utf-8', newline="\n") as file:
-        for item in pure:
-            file.write(f'{item}\n')
-
-
-def hasher(directory):
-    desc = 'Hashing Captchas'
-    files = os.listdir(directory)
-    md5hashes = []
-    for file in tqdm(files, desc=desc):
-        if '~' not in file:
-            md5hash = hashlib.md5(Image.open(os.path.join(directory,file)).tobytes())
-            md5hashes.append(md5hash.hexdigest()+'.'+file[:6])
-    with open('/home/gabriel/pythonCode/RevTec/captcha_hashes.txt', mode='w', encoding='utf-8') as hash_file:
-        data_to_write = [f'{i}\n' for i in md5hashes]
-        hash_file.writelines(data_to_write)
-    print(f'Total unique hashes: {len(data_to_write):,} out of {len(files):,} files.')
-
+def cleaner():
+    # Erase all content from 'temp' folder
+    for file in os.listdir(RUN.temp_dir):
+        os.remove(os.path.join(RUN.temp_dir, file))
 
 def main_loop():
     this_thread = int(thread)
     for k, placa in enumerate(list_of_pending_placas(str(this_thread))):
-        run.monitor[this_thread] += 1
-        if k == iterations:
-            return
-        try:
-            scrape = extract(placa, run.url, str(this_thread))
-            resultado = analizar_respuesta(placa, scrape)
-            add_to_file('resultados'+str(this_thread)+'.txt', resultado)
-        except KeyboardInterrupt:
-            quit()
-        except:
-            run.exceptions += 1
-            print(f'Exception Thread: {this_thread}')
-
+        if valid_placa(placa):
+            RUN.monitor[this_thread] += 1
+            try:
+                scrape = extract(placa, RUN.url, str(this_thread))
+                resultado = analizar_respuesta(placa, scrape)
+                RUN.results[this_thread].append(resultado)
+            except KeyboardInterrupt:
+                quit()
+            except:
+                RUN.exceptions += 1
+    RUN.done[this_thread] = True
 
 
 # Start Program
-
 if __name__ == '__main__':
-
+    # Validate arguments for Threads (sessions), Iterations and Cycles
+    if len(sys.argv) < 3:
+        raise Exception('Need at least three arguments. Format: scraper.py #THREADS #SEARCHES per thread #CYCLES')
     # Init variables
-    IMAGE_FOLDER = '/home/gabriel/pythonCode/RevTec/images'
     start_time = dt.now()
     sessions = int(sys.argv[1])
     iterations = int(sys.argv[2])
-    run = Basics(sessions)
-
-    # Checks for file that indicates completed previous run
-    if os.path.exists('/home/gabriel/pythonCode/RevTec/completed.sig'):
-
-        # Remove file that indicates process complete
-        os.remove('/home/gabriel/pythonCode/RevTec/completed.sig')
-    
+    reader = easyocr.Reader(['en'], gpu=False)
+    # Start cycling
+    for repeat in range(int(sys.argv[3])):
+        print(f'Cycle: {repeat+1} of {sys.argv[3]}')
+        RUN = Basics(sessions)
         # Print current state    
-        dashboard(0, opt=False)
-
-        if len(sys.argv) < 3:
-            raise Exception('Need two arguments')
-
+        dashboard(0, exit=False)
         # Take main input file with placas and split into files with max_placas each
-        split(sessions)
-
+        split(sessions, iterations)
         # Create threads to extract in parallel
         all_threads = []
         for thread in range(sessions):
             new_thread = threading.Thread(target=main_loop)
             all_threads.append(new_thread)
             new_thread.start()
-
-        # Ensure all threads end before moving forward
+        # Gives control to monitor and ensures all threads end before moving forward
         thread_monitor(sessions, iterations)
-        #_ = [i.join() for i in all_threads]
-        
         # Join all split result files into big one
-        print('Consolidando bases de datos')
-        os.system('cat resultados*.txt >> todo_resultados.txt')
-
-    else:
-        print('Completing previous run')
-        sessions = len([i for i in os.listdir('/home/gabriel/pythonCode/RevTec/') if 'resultados' in i]) - 1
-
-    # Clean placas split files by removing processed placas
-    consolidate(sessions)
-
-    # Join all split placas files into big one
-    os.system('cat placas*.txt > todo_placas.txt')
-
-    # Erase all temporary files
-    os.system('rm captcha*.jpg')
-    os.system('rm resultados*.txt')
-    os.system('rm placas*.txt')
-
-    # Clean files and directories
-    cleaner('todo_placas.txt')
-
-    # Create hash for all images into txt file
-    hasher(IMAGE_FOLDER)
-
+        print('\nConsolidando bases de datos')
+        # Take results and integrate them into existing results
+        consolidate()
+        # Clean temp files
+        cleaner()
     # Backup main files in Google Drive
     if 'NOBACK' not in sys.argv:
         print('Backing up in Google Drive')
-        try:
-            os.system('rclone copy todo_resultados.txt gDrive:/TechData/RevTec')
-            os.system('rclone copy todo_placas.txt gDrive:/TechData/RevTec')
-            os.system('rclone copy captcha_hashes.txt gDrive:/TechData/RevTec')
-        except:
-            print('Error with GDrive backup!')
-
-    # Close with stats
+        os.system('rclone copy todo_resultados.txt gDrive:/TechData/RevTec')
+        os.system('rclone copy todo_placas.txt gDrive:/TechData/RevTec')
+    # Close showing stats
     dashboard(start_time)
-
-    # Normal exit... write file that marks as completed
-    with open('/home/gabriel/pythonCode/RevTec/completed.sig', 'w') as file:
-        file.write(' ')
